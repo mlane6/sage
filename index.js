81fb14ebb79c4e39a60215cc721e975f93573ac7
Brad Waropay <brad.waropay@gmail.com>
2018-01-15 13:36:59 -0700

    Add build filters

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const Metalsmith = require('metalsmith');
-const autotoc = require('metalsmith-autotoc');
-const markdown = require('metalsmith-markdown');
-const permalinks = require('metalsmith-permalinks');
 const collect = require('metalsmith-auto-collections')
+const markdown = require('metalsmith-markdown');
+const autotoc = require('metalsmith-autotoc');
+const filter = require('metalsmith-filter');
+const permalinks = require('metalsmith-permalinks');
 const layouts = require('metalsmith-layouts');
 
 Metalsmith(__dirname)
@@ -14,14 +15,19 @@ Metalsmith(__dirname)
   })
   .source('./src')
   .destination('./build')
+
   .use(collect({
     pattern: ['*.md', '**/*.md', '!index.md']
   }))
+  .use(filter([
+    '*',
+    '!src/**/*.md'
+  ]))
   .use(markdown())
-  .use(permalinks())
   .use(autotoc(
     {selector: 'h2, h3'}
   ))
+  .use(permalinks())
   .use(layouts({
     directory: 'layout',
     engine: 'pug',
